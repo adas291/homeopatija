6ba02dd8b1fab1efd4a278ba7d0c523b6bfaac19
Nenumarokas <marius.pazera@gmail.com>
2023-12-23 10:10:23 +0200

    🤝

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -10,88 +10,97 @@ using homeopatija.Repos;
 [Route("Comments")]
 public class CommentsController : Controller
 {
-  private readonly HomeopatijaContext _db;
-  private readonly CommentRepo _commentRepo;
-  public CommentsController(HomeopatijaContext db, CommentRepo commentRepo)
-  {
-    _db = db;
-    _commentRepo = commentRepo;
-  }
-
-  [HttpPost("Update")]
-  public IActionResult Update(Comment comment)
-  {
-    //to be fixed when authentication implemented
-    comment.UserId = 1;
-
-    var result = _commentRepo.UpdateComment(comment);
-    if(result == 1)
+    private readonly HomeopatijaContext _db;
+    private readonly CommentRepo _commentRepo;
+    public CommentsController(HomeopatijaContext db, CommentRepo commentRepo)
     {
-      TempData["StatusMessage"] = "Komentaras atnaujintas sėkmingai";
-      return RedirectToAction("GetProductView", "Drug", new { id = comment.DrugId});
-    }
-    else {
-      TempData["StatusMessage"] = "Komentaras nebuvo sukurtas";
-      return BadRequest();
+        _db = db;
+        _commentRepo = commentRepo;
     }
 
-  }
-  [HttpPost("Create")]
-  public IActionResult Create(Comment comment)
-  {
-    var result = _commentRepo.CreateComment(comment);
-    if(result == 1)
+    [HttpPost("Update")]
+    public IActionResult Update(Comment comment)
     {
-      TempData["StatusMessage"] = "Komentaras sukurtas sėkmingai";
-      return RedirectToAction("GetProductView", "Drug", new { id = comment.DrugId});
-    }
-    else {
-      TempData["StatusMessage"] = "Komentaras nebuvo sukurtas";
-      return BadRequest();
-    }
+        //to be fixed when authentication implemented
+        comment.UserId = 1;
 
-  }
+        var result = _commentRepo.UpdateComment(comment);
 
-  [HttpDelete("{id}")]
-  public IActionResult Delete(int id)
-  {
-    var result = _commentRepo.Delete(id);
-    if(result == 1)
+        if (result == 1)
+        {
+            TempData["StatusMessage"] = "Komentaras atnaujintas sėkmingai";
+            return RedirectToAction("GetProductView", "Drug", new { id = comment.DrugId });
+        }
+        else
+        {
+            TempData["StatusMessage"] = "Komentaras nebuvo sukurtas";
+            return RedirectToAction("GetProductView", "Drug", new { id = comment.DrugId });
+        }
+
+    }
+    [HttpPost("Create")]
+    public IActionResult Create(Comment comment)
     {
-      TempData["StatusMessage"] = "Komentaras pašalintas sėkmingai";
-      return Ok(TempData["StatusMessage"]);
-    }
-    else {
-      TempData["StatusMessage"] = "Komentaras nebuvo pašalintas";
-      return BadRequest();
-    }
-  }
+        var result = _commentRepo.CreateComment(comment);
+        if (result == 1)
+        {
+            TempData["StatusMessage"] = "Komentaras sukurtas sėkmingai";
+            return RedirectToAction("GetProductView", "Drug", new { id = comment.DrugId });
+        }
+        else
+        {
+            TempData["StatusMessage"] = "Komentaras nebuvo sukurtas";
+            return RedirectToAction("GetProductView", "Drug", new { id = comment.DrugId });
 
-  [HttpPost("Report")]
-  public async Task<IActionResult> Report(Report report)
-  {
-    report.User = null;
+            //return BadRequest();
+        }
 
-    var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-    if (userId != null) {
-      report.UserId = Convert.ToInt32(userId);
-    }
-    else {
-      report.UserId = 1;
     }
 
-    var created = await _commentRepo.CreateReport(report);
-
-    if(created)
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
     {
-      // TempData["StatusMessage"] = "Pranešimas išsiųstas administratoriui";
-      // return RedirectToAction("GetProductView", "Drug", new { id = report.CommentId});
-      return Ok();
+        var result = _commentRepo.Delete(id);
+        if (result == 1)
+        {
+            TempData["StatusMessage"] = "Komentaras pašalintas sėkmingai";
+            return Ok(TempData["StatusMessage"]);
+        }
+        else
+        {
+            TempData["StatusMessage"] = "Komentaras nebuvo pašalintas";
+            return BadRequest();
+        }
     }
-    else {
-      // TempData["StatusMessage"] = "Įvyko klaida";
-      // return RedirectToAction("GetProductView", "Drug", new { id = report.CommentId});
-      return BadRequest();
+
+    [HttpPost("Report")]
+    public async Task<IActionResult> Report(Report report)
+    {
+        report.User = null;
+
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId != null)
+        {
+            report.UserId = Convert.ToInt32(userId);
+        }
+        else
+        {
+            report.UserId = 1;
+        }
+
+        var created = await _commentRepo.CreateReport(report);
+
+        if (created)
+        {
+            // TempData["StatusMessage"] = "Pranešimas išsiųstas administratoriui";
+            // return RedirectToAction("GetProductView", "Drug", new { id = report.CommentId});
+            return Ok();
+        }
+        else
+        {
+            // TempData["StatusMessage"] = "Įvyko klaida";
+            // return RedirectToAction("GetProductView", "Drug", new { id = report.CommentId});
+            return BadRequest();
+        }
     }
-  }
 }
